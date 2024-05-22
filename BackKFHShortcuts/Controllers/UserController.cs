1299999ebc24884fc24d949e42c55e3fa9ef555d
ytmandani <ytmandani@outlook.com>
2024-05-22 13:27:57 +0300

    Added add produc and add category endpoints

---
+++ BackKFHShortcuts/Controllers/UserController.cs
@@ -0,0 +1,57 @@
+﻿using BackKFHShortcuts.Models;
+using BackKFHShortcuts.Models.Entities;
+using BackKFHShortcuts.Models.Responses;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackKFHShortcuts.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ShortcutsContext _context;
+
+        public UserController(ShortcutsContext shortcutsContext)
+        {
+            _context = shortcutsContext;
+        }
+
+        [ProducesResponseType(typeof(Category), 200)]
+        [HttpGet("GetCategory")]
+        public async Task<ActionResult<IEnumerable<Category>>> getCategories()
+        {
+            using(var context = _context)
+            {
+                return Ok(await context.Categories.Select(x => new GetCategoryResponse { Id = x.Id, Name = x.Name }).ToListAsync());
+            }
+        }
+
+        [ProducesResponseType(typeof(Product), 200)]
+        [HttpGet("GetProduct")]
+        public async Task<ActionResult<IEnumerable<Product>>> getProducts(string category)
+        {
+            using( var context = _context)
+            {
+                return Ok(await context.Products
+                    .Include(x => x.Category)
+                    .Where(x => x.Category.Name == category)
+                    .Select(x => new GetProductResponse
+                    {
+                        Id = x.Id,
+                        Name = x.Name,
+                        Image = x.Image,
+                        Shariah = x.Sharia,
+                        TargetAudience = x.TargetAudience,
+                        Description = x.Description,
+                        CategoryName = x.Category.Name,
+                    }).ToListAsync());
+            }
+        }
+
+        
+
+    }
+}
---
+++ BackKFHShortcuts/Models/Responses/GetCategoryResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace BackKFHShortcuts.Models.Responses
+{
+    public class GetCategoryResponse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
