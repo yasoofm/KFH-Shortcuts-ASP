e26e57249985ca1421e7b2ee3a5e9a3fd5592102
Saleh-Binessa <saleh.binessa@gmail.com>
2024-06-02 11:08:40 +0300

    SCRUM-61 Dashboard design updated

---
+++ FrontKFHShortcuts/Models/Dashboard/DashboardRequest.cs
@@ -0,0 +1,24 @@
+﻿namespace FrontKFHShortcuts.Models.Dashboard
+{
+    public class DashboardRequest
+    {
+        public int TotalRequests { get; set; }
+        public List<RequestPerMonth> MonthlyRequests { get; set; }
+        public List<RequestPerProduct> ProductRequests { get; set; }
+        public List<RequestPerProduct> TopProducts { get; set; }
+        public List<RequestPerProduct> LeastProducts { get; set; }
+    }
+
+    public class RequestPerMonth
+    {
+        public string Month { get; set; }
+        public int Requests { get; set; }
+    }
+
+    public class RequestPerProduct
+    {
+        public string Image { get; set; }
+        public string ProductName { get; set; }
+        public int Requests { get; set; }
+    }
+}
