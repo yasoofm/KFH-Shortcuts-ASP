b0854617776a1c28eb900169496ccfaafbea2537
ytmandani <ytmandani@outlook.com>
2024-06-09 09:54:51 +0300

    added system messages to train ai

---
+++ BackKFHShortcuts/Models/Entities/SystemMessage.cs
@@ -0,0 +1,9 @@
+﻿namespace BackKFHShortcuts.Models.Entities
+{
+    public class SystemMessage
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public string Role { get; set; }
+    }
+}
